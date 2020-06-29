4c7575969848876589494f58a9c1a644ec909d29
JewEllerrr <55089823+JewEllerrr@users.noreply.github.com>
2020-06-29 17:18:32 +0300

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -4,17 +4,18 @@
 
 int main() 
 {
-	BCH code(15,8);
-	vector<bool> mes = {1,0,1};
+	BCH code(15,5);
+	vector<bool> mes = {1,0,1,1};
 	//vector<bool> a = { 1,1,0,0,1,0,0,0,1,1,1,1,0,1,0 };
 	vector<bool> a = code.coding(mes, code.getG());
 	cout << "Vector a:" << endl;
 	print(a);
-	vector<bool> e = {1,0,0,0,1,0,0,0 };
+	vector<bool> e = {0,0,1,0,0,0,0,1 };
 	vector<bool> b = vector_b(a, e);
 	cout << "Vector b = a + e:" << endl;
 	print(b);
 	vector<bool> res = code.decoding(b);
+
 	cout << "Decoded received vector:" << endl;
 	print(res);
 	cout << "Vector a:" << endl;
@@ -22,4 +23,3 @@ int main()
 
 	return 0;
 }
-
